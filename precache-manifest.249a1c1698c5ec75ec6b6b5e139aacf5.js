3c66edb4fe071ed197bcabc6e46f7e9d3cf75f8a
Mustafiz <js.mustafiz@gmail.com>
2020-08-24 10:41:14 +0600

    Updates

---
+++ precache-manifest.249a1c1698c5ec75ec6b6b5e139aacf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f615f465614c9c359d9cadb34195ec4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f7a242260c44aeef6c2",
+    "url": "/static/css/main.deb46ce5.chunk.css"
+  },
+  {
+    "revision": "33ad4236f5cc6f21f242",
+    "url": "/static/js/2.e14039ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e14039ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7a242260c44aeef6c2",
+    "url": "/static/js/main.0122e5c4.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/netflix-clone-react/precache-manifest.e7b05924ffca2cd4f533f3de56e2f0af.js"
+  "/precache-manifest.249a1c1698c5ec75ec6b6b5e139aacf5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/netflix-clone-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
